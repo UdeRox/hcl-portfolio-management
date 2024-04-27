62b79d80211c0473b441bb0fe882578e1c5d06fb
udeshika <perera.n.u@gmail.com>
2024-04-27 15:01:39 +0800

    Adding ApiGatea

---
+++ api-gateway/src/test/java/com/ude/hcl/gateway/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ude.hcl.gateway.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class ApiGatewayApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
